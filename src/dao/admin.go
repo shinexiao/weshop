da995fdceac4f252db87d8e203a43c13440db04e
shine.xiao <shine@easymin.com>
2018-06-06 21:02:40 +0800

    新增管理员数据库访问层

--- src/dao/admin.go
+++ src/dao/admin.go
@@ -1,1 +1,18 @@
 package dao
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// 管理员数据库访问对象
+type Admin struct {
+	gorm.Model
+	Username string `gorm:"column:username;type:char(20);unique;not null"` // 用户名
+	Password string `gorm:"column:password;type:char(30);not null"`        // 密码，密码经过sha1加密
+}
+
+// 定义admin表名
+func (Admin) TableName() string {
+
+	return "shop_admin"
+}
