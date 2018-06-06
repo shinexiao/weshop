e083e145d60e489d0eeba6a460087444cbc6d127
fzh <359096209@qq.com>
2018-06-06 21:04:42 +0800

    订单模型

---
+++ src/dao/order.go
@@ -0,0 +1,17 @@
+package dao
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// 订单模型
+type Order struct {
+	gorm.Model
+	OrderNo string `gorm:column:orderNo` //单号
+	Status  int8   `gorm:column:status`  // 状态
+}
+
+// 返回表名
+func (Order) TableName() string {
+	return "shop_order"
+}
