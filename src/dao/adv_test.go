a08c8630bfe18a5aa6f6c24dcd58fb61e1c00bdf
陈哲宇 <1452100100@qq.com>
2018-06-07 20:01:54 +0800

    优化 新闻

--- src/dao/adv_test.go
+++ src/dao/adv_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"pojo/model"
+	"os"
 )
 
 // 单元测试，测试advDao对象的构造
@@ -22,7 +23,7 @@ func TestNewAdvDao(t *testing.T) {
 // 单元测试，测试保存adv对象
 func TestAdvDao_InsertAdv(t *testing.T) {
 	// 连接数据库
-	db, _ := gorm.Open("sqlite3", "/tmp/gorm.db")
+	db, _ := gorm.Open("sqlite3", "adv_test_gorm.db")
 
 	// 创建数据库表
 	db.CreateTable(model.Adv{})
@@ -39,12 +40,25 @@ func TestAdvDao_InsertAdv(t *testing.T) {
 	// 把adv实例 保存入数据库
 	result, err := advDao.InsertAdv(adv)
 
-	// 关闭数据库
-	db.Close()
-
 	// 断言 错误信息
 	assert.Equal(t, err, nil)
 
 	// 断言 正确信息
 	assert.Equal(t, result, int64(1))
+
+	adv = &model.Adv{
+		Img: "图片",
+		Url: "地址",
+	}
+
+	// 把重复的adv实例 保存入数据库 测试保存
+	_, err = advDao.InsertAdv(adv)
+
+	// 断言 错误信息
+	assert.Equal(t, err, nil)
+
+	// 关闭数据库
+	db.Close()
+
+	os.Remove("adv_test_gorm.db")
 }
