5e2fa7d2ea79b417ad56687243d30068a5072a5b
陈哲宇 <1452100100@qq.com>
2018-06-08 11:08:31 +0800

    新闻增加增删改查

--- src/dao/news.go
+++ src/dao/news.go
@@ -22,13 +22,6 @@ func (this *NewsDao) InsertNews(news *model.News) (int64, error) {
 	// 把新闻信息插入数据库 返回一个信息
 	result := this.db.Create(&news)
 
-	this.db.Table().Where("id = ?", id).Update(
-	{
-		map[string]interface{}{
-			"title", news.Title,
-		}
-	})
-
 	// 判断是否有错误信息
 	if result.Error != nil {
 		// 保存出错了
@@ -40,16 +33,43 @@ func (this *NewsDao) InsertNews(news *model.News) (int64, error) {
 }
 
 // 查询新闻信息
-func (this *NewsDao) FirstNews(news *model.News, page, limit int32) (int64, error) {
+func (this *NewsDao) GetNewsList(news *model.News, page, limit int32) ([]*model.News, error) {
+	news_list := make([] *model.News, limit)
 	// 把新闻信息更新到数据库  返回一个信息
-	result := this.db.Table("news").Where("title like %?%", news.Title).Order("title DESC").Limit(20).Offset((page - 1) * limit).Find(news)
+	result := this.db.Table("shop_news").Where(map[string]interface{}{"title": news.Title}).Order("created_at").Limit(limit).Offset((page - 1) * limit).Find(&news_list)
 
 	// 判断是否有错误信息
 	if result.Error != nil {
 		// 保存出错了
-		return 0, result.Error
+		return nil, result.Error
 	}
 
 	// 成功返回 行数
+	return news_list, nil
+}
+
+// 根据ID查询新闻
+func (this *NewsDao) GetNews(id int64) (*model.News, error) {
+	news := &model.News{}
+
+	result := this.db.Table("shop_news").Where("id = ?", id).Find(news)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return news, nil
+}
+
+// 根据id 修改对于的 新闻
+func (this *NewsDao) PutNews(news *model.News, id int64) (int64, error) {
+	result := this.db.Table("shop_news").Where("id = ?", id).Updates(map[string]interface{}{
+		"title":   news.Title,
+		"content": news.Content,
+		"img":     news.Img,
+	})
+	if result.Error != nil {
+		return 0, result.Error
+	}
 	return result.RowsAffected, nil
 }
--- src/dao/news_test.go
+++ src/dao/news_test.go
@@ -5,8 +5,7 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"pojo/model"
-	"fmt"
-	"runtime/debug"
+	"os"
 )
 
 // 单元测试，测试newsDao对象的构造
@@ -20,90 +19,131 @@ func TestNewsDao_InsertNews(t *testing.T) {
 	// 连接 数据库
 	db, _ := gorm.Open("sqlite3", "news_test_gorm.db")
 
-	//创建 数据库表
-	//db.CreateTable(model.News{})
+	// 关闭 数据库
+	defer db.Close()
 
-	// 创建 一个新闻对象
-	news := make([] *model.News, 2)
-	new1 := &model.News{
+	//创建 数据库表
+	db.CreateTable(model.News{})
+
+	newsDao := NewNewsDao(db)
+
+	news := &model.News{
 		Title:   "成都新闻",
 		Content: "成都新闻内容",
 		Img:     "成都新闻图片",
 	}
-	new2 := &model.News{
+	// 正确测试
+	result, err := newsDao.InsertNews(news)
+
+	// 断言 错误信息
+	assert.Nil(t, err)
+
+	// 断言 正确信息
+	assert.Equal(t, result, int64(1))
+
+	// 错误测试
+	news = &model.News{
+		Title:   "成都新闻1",
+		Content: "成都新闻内容1",
+		Img:     "成都新闻图片1",
+	}
+	_, err = newsDao.InsertNews(news)
+	assert.NotNil(t, err)
+
+	//os.Remove("news_test_gorm.db")
+}
+
+// 测试 根据id 获取单个新闻
+func TestNewsDao_GetNews(t *testing.T) {
+	db, _ := gorm.Open("sqlite3", "news_test_gorm.db")
+
+	//删除数据库表
+	defer os.Remove("news_test_gorm.db")
+
+	//关闭数据库表
+	defer db.Close()
+
+	// 创建数据库表
+	db.CreateTable(model.News{})
+
+	// 创建实例
+	newsDao := NewNewsDao(db)
+
+	// 新建一个新闻
+	news := &model.News{
+		Title:   "重庆新闻",
+		Content: "重庆新闻内容",
+		Img:     "重庆新闻图片",
+	}
+
+	// 保存一个 新闻
+	result, err := newsDao.InsertNews(news)
+
+	assert.Nil(t, err)
+	assert.Equal(t, result, int64(1))
+
+	// 正确测试
+	news, err = newsDao.GetNews(1)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, news.Title, "重庆新闻")
+	assert.Equal(t, news.Content, "重庆新闻内容")
+	assert.Equal(t, news.Img, "重庆新闻图片")
+
+	//错误测试
+	news, err = newsDao.GetNews(2)
+	assert.NotNil(t, err)
+}
+
+// 测试 根据ID 修改单个新闻
+func TestNewsDao_PutNews(t *testing.T) {
+	// 连接数据库
+	db, _ := gorm.Open("sqlite3", "news_test_gorm.db")
+
+	//删除数据库表
+	defer os.Remove("news_test_gorm.db")
+
+	//关闭数据库表
+	defer db.Close()
+
+	// 创建数据库表
+	db.CreateTable(model.News{})
+
+	// 创建实例
+	newsDao := NewNewsDao(db)
+
+	// 新建一个新闻
+	news := &model.News{
+		Title:   "重庆新闻",
+		Content: "重庆新闻内容",
+		Img:     "重庆新闻图片",
+	}
+
+	// 向数据添加一个新闻
+	result, err := newsDao.InsertNews(news)
+
+	assert.Nil(t, err)
+	assert.Equal(t, result, int64(1))
+
+	news = &model.News{
 		Title:   "北京新闻",
 		Content: "北京新闻内容",
 		Img:     "北京新闻图片",
 	}
 
-	news[0] = new1
-	news[1] = new2
-
-	for key, val := range news {
-		fmt.Print(key, val)
-	}
-
-	// 构造 新闻数据库对象
-	newsDao := NewNewsDao(db)
-
-	// 新闻 保存事件
-	result, err := newsDao.InsertNews(new1)
-	result, err = newsDao.InsertNews(new2)
-
-	// 关闭 数据库
-	db.Close()
-
-	// 断言 错误信息
-	assert.Equal(t, err, nil)
-
-	// 断言 正确信息
+	// 正确测试
+	result, err = newsDao.PutNews(news, 1)
+	assert.Nil(t, err)
 	assert.Equal(t, result, int64(1))
 
-	//os.Remove("news_test_gorm.db")
-}
+	// 获取最新的 id 1 的新闻
+	db.Table("shop_news").Where("id = ?", 1).Find(news)
 
-func TestNewsDao_FirstNews(t *testing.T) {
-	defer func()(){
-		if err := recover();nil != err {
-			debug.PrintStack()
-		}
-	}()
-	// 连接 数据库
-	db, _ := gorm.Open("sqlite3", "news_test_gorm.db")
+	assert.Equal(t, news.Title, "北京新闻")
+	assert.Equal(t, news.Content, "北京新闻内容")
+	assert.Equal(t, news.Img, "北京新闻图片")
 
-	//创建 数据库表
-	//db.CreateTable(model.News{})
-
-	// 创建 一个新闻对象
-	var news []model.News
-
-	// 构造 新闻数据库对象
-	//newsDao := NewNewsDao(db)
-	//
-	//// 新闻 保存事件
-	//result, err := newsDao.InsertNews(news)
-	//
-	//// 创建 一个新闻对象
-	//news = &model.News{
-	//	Title:   "新闻表头2",
-	//	Content: "新闻内容2",
-	//	Img:     "新闻图片2",
-	//}
-	//
-	//// 新闻 保存事件
-	//result, err = newsDao.InsertNews(news)
-
-	db.Limit(10).Offset(0).Find(&news)
-
-	fmt.Print(news)
-	// 关闭 数据库
-	defer db.Close()
-
-	// 断言 错误信息
-	//assert.Equal(t, err, nil)
-	//
-	//// 断言 正确信息
-	//assert.Equal(t, result, int64(1))
-
-	//os.Remove("news_test_gorm.db")
+	// 错误测试
+	result, err = newsDao.PutNews(news, 12)
+	assert.Nil(t, err)
+	assert.Equal(t, result, int64(0))
 }
