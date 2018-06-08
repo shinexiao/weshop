739f67587139e43f244ab06108adf2ea033c8be5
zzpp515630 <1219908618@qq.com>
2018-06-08 22:00:55 +0800

    活动管理完善单元测试

--- src/dao/activity_test.go
+++ src/dao/activity_test.go
@@ -6,8 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"pojo/model"
+	_ "os"
+	"fmt"
+	"math/rand"
+	"time"
 	"os"
 )
+
+/**
+单元测试 测试数据库连接
+ */
 func TestNewActiveDao(t *testing.T) {
 	var db *gorm.DB
 
@@ -16,20 +24,121 @@ func TestNewActiveDao(t *testing.T) {
 	assert.Equal(t, db, activety.db)
 }
 
+/**
+单元测试 测试批量添加操作
+ */
 func TestActivetyDao_InsterActivity(t *testing.T) {
-
 	db, _ := gorm.Open("sqlite3", "unit_test_gorm.db")
 	//创建表
 	db.CreateTable(model.Activity{})
 
 	activity := &model.Activity{
-		Type:1,
-		NewCash:12.0,
-		OldCash:11.0,
+		Type:    1,
+		NewCash: 12.0,
+		OldCash: 11.0,
 	}
 	dao := NewActivityDao(db)
-	result ,err:= dao.InsterActivity(activity)
-	assert.Equal(t,err,nil)
-	assert.Equal(t,result,int64(1))
-	os.Remove("unit_test_gorm.db")
+	result, err := dao.InsterActivity(activity)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, result, int64(1))
+	//批量插入数据库
+	for i := 0.01; i < 0.1; i = i + 0.01 {
+		activity = &model.Activity{
+			Type:        2,
+			OneRebate:   0.1 + i,
+			TwoRebate:   0.2 + i,
+			ThreeRebate: 0.3 + i,
+			StartTime:   time.Now(),
+			EndTime:     time.Now(),
+		}
+		result, err = dao.InsterActivity(activity)
+		fmt.Println(result, i)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, int64(1), result)
+	}
+	//os.Remove("unit_test_gorm.db")
+}
+
+/**
+单元测试 测试批量查询
+ */
+func TestActivityDao_QueryActivity(t *testing.T) {
+	db, _ := gorm.Open("sqlite3", "unit_test_gorm.db")
+	dao := NewActivityDao(db)
+	query := &model.ActivityQY{
+		Type: 2,
+		Page: 1,
+		Size: 10,
+		GoodsId:15,
+		StartTime:time.Unix(time.Now().Unix()-10000,2222),
+		EndTime:time.Unix(time.Now().Unix()+10000,2222),
+	}
+	activities, e := dao.QueryActivity(query)
+	fmt.Println(e)
+	assert.Equal(t, e, nil)
+	assert.Equal(t, true, len(activities) > 0)
+	//fmt.Println(activities)
+	for _, activity := range activities {
+		fmt.Println(activity)
+	}
+}
+
+/*
+单元测试 测试通过id查询
+ */
+func TestActivityDao_QueryActivityById(t *testing.T) {
+	db, _ := gorm.Open("sqlite3", "unit_test_gorm.db")
+	dao := NewActivityDao(db)
+	//产生随机数字
+	rand.Seed(time.Now().Unix())
+	intn := rand.Intn(10)
+	activity, e := dao.QueryActivityById(uint64(intn + 1))
+	assert.Equal(t, e, nil)
+	assert.NotEqual(t, activity, nil)
+}
+
+/**
+单元测试 测试修改参数
+ */
+func TestActivityDao_UpdateActivity(t *testing.T) {
+	db, _ := gorm.Open("sqlite3", "unit_test_gorm.db")
+	dao := NewActivityDao(db)
+	//产生随机数字
+	rand.Seed(time.Now().Unix())
+	intn := rand.Intn(10)
+	fmt.Println(intn)
+	activity := &model.Activity{
+		GoodsId: 15,
+	}
+	updateActivity, e := dao.UpdateActivity(activity)
+	assert.Equal(t, nil, e)
+	assert.Equal(t, int64(11), updateActivity)
+
+	id, e := dao.QueryActivityById(uint64(intn+1))
+	assert.Equal(t, nil, e)
+	fmt.Println(id)
+	assert.NotEqual(t, nil, id)
+}
+
+/**
+单元测试 删除操作
+ */
+func TestActivityDao_DeleteActivity(t *testing.T) {
+	db, _ := gorm.Open("sqlite3", "unit_test_gorm.db")
+	dao := NewActivityDao(db)
+	//产生随机数字
+	rand.Seed(time.Now().Unix())
+	intn := rand.Intn(10)
+	fmt.Println(intn)
+	i, e := dao.DeleteActivity(int64(intn+1))
+	assert.Equal(t, nil, e)
+	assert.NotEqual(t, 1, i)
+
+	// 数据查询
+	id,err := dao.QueryActivityById(uint64(intn+1))
+	fmt.Println(id,err)
+	assert.NotEqual(t, err, nil)
+	assert.NotEqual(t, id, nil)
+	// 删除数据库
+	defer os.Remove("unit_test_gorm.db")
 }
--- src/pojo/model/activity.go
+++ src/pojo/model/activity.go
@@ -1,20 +1,45 @@
 package model
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
 
-import "github.com/jinzhu/gorm"
-
+/**
+活动结构体
+ */
 type Activity struct {
 	gorm.Model
-	NewCash     float64 `gorm:"column:new_cash;type:double(64);null" json:"new_cash"`         //新客户返现金卷
-	OldCash     float64 `gorm:"column:old_cash;type:double(64);null" json:"old_cash"`         //老客户返现金卷
-	Type        int32   `gorm:"column:type;type:int(1);not null" json:"type"`                 //1、推广有礼 or  2、购买返现
-	OneRebate   float32 `gorm:"column:one_rebate;type:double(64);null" json:"one_rebate"`     //第一级返利
-	FourRebate  float32 `gorm:"column:four_rebate;type:double(64);null" json:"four_rebate"`   //第二级返利
-	ThreeRebate float32 `gorm:"column:three_rebate;type:double(64);null" json:"three_rebate"` //第三级返利
-	GoodsId     uint64  `gorm:"column:goods_id;type:bigint(32);null" json:"goods_id"`         //商品id
+	NewCash     float64   `gorm:"column:new_cash;type:double(64);null" json:"new_cash"`         // 新客户返现金卷
+	OldCash     float64   `gorm:"column:old_cash;type:double(64);null" json:"old_cash"`         // 老客户返现金卷
+	Type        int32     `gorm:"column:type;type:int(1);not null" json:"type"`                 // 1、推广有礼 or  2、购买返现
+	OneRebate   float64   `gorm:"column:one_rebate;type:double(64);null" json:"one_rebate"`     // 第一级返利
+	TwoRebate   float64   `gorm:"column:two_rebate;type:double(64);null" json:"twos_rebate"`    // 第二级返利
+	ThreeRebate float64   `gorm:"column:three_rebate;type:double(64);null" json:"three_rebate"` // 第三级返利
+	GoodsId     uint64    `gorm:"column:goods_id;type:bigint(32);null" json:"goods_id"`         // 商品id
+	StartTime   time.Time `json:"start_time"`        // 活动开始时间
+	EndTime     time.Time `json:"end_time"`            // 活动结束时间
+	//StartTime   time.Time `gorm:"column:start_time;type:datatime;null json:"start_time"`        // 活动开始时间
+	//EndTime     time.Time `gorm:"column:end_time;type:datatime;null json:"end_time"`            // 活动结束时间
 }
 
-//设置表名称
-func (Activity)TableName() string {
+/**
+查询活动结构体
+ */
+type ActivityQY struct {
+	gorm.Model
+	GoodsName string    `json:"goods_name" form:"goods_name"` // 商品名称
+	Type      int32     `json:"type" form:"type"`             // 1、推广有礼 or  2、购买返现
+	GoodsId   uint64    `json:"goods_id" form:"goods_id"`     // 商品id
+	Page      int64     `json:"page" form:"page" `            // 分页
+	Size      int64     `json:"size" form:"size"`             // 分页
+	StartTime time.Time `json:"start_time" form:"start_time"` // 活动开始时间
+	EndTime   time.Time `json:"end_time" form:"end_time"`     // 活动结束时间
+}
+
+/**
+指定数据库表名称
+ */
+func (Activity) TableName() string {
 	return "activity"
 }
